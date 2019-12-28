2557f8ee5d9666a94654c14b5896177f21861d30
V0LTaiR <57043344+V0LTaiR@users.noreply.github.com>
2019-12-28 14:38:45 +0400

    Update model_mail.php

--- test/application/models/model_mail.php
+++ test/application/models/model_mail.php
@@ -4,35 +4,29 @@ class Model_Mail extends Model
 {
     public function validSend($mail = null, $name = null, $theme = null, $text = null)
     {
-        $result = true;
+        $result = "";
         if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-            $result = 'ошибка! Email указан некорректно';
-            return $result;
+            $result .= 'ошибка! Email указан некорректно</br>'. PHP_EOL;
         }
 
         if (empty(trim($name))) {
-            $result = 'ошибка! Вы не представились!';
-            return $result;
+            $result .= 'ошибка! Вы не представились!</br>' . PHP_EOL;
         }
 
         if (empty(trim($theme))) {
-            $result = 'ошибка! Вы не указали тему письма';
-            return $result;
+            $result .= 'ошибка! Вы не указали тему письма</br>'.PHP_EOL;
         }
 
         if (empty(trim($text))) {
-            $result = 'ошибка! Вы не ввелиb сообщение';
-            return $result;
+            $result .= 'ошибка! Вы не ввели сообщение</br>' . PHP_EOL;
         }
 
-        if ($result === true) {
+        if ($result === "") {
             $text = "Вам было отправлено сообщение от пользователя $name ($mail)    
 
 $text";
             $result = mail('admin@site.ru', $theme, $text) ? 'письмо успешно отправлено' : 'неизвестная ошибка при отправке!';
-            
         }
-        
-		return $result;
+        return $result;
     }
 }
